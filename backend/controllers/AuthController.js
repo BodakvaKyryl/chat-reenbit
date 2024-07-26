e56568fc9e3a6eba91c3cfba631b2cde813d306c
Kyryl <bodakvakyryl@gmail.com>
2024-07-26 18:32:09 +0300

    Added login to AuthController

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -7,14 +7,16 @@ authController.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    if (!username || !email || !password) {
+    if (
+      req.body.username === '' ||
+      req.body.email === '' ||
+      req.body.password === ''
+    ) {
       return res.status(400).json({ msg: 'All fields are required' });
     }
 
     const isExist = await User.findOne({ email });
-    if (isExist) {
-      return res.status(400).json({ msg: 'User already exists' });
-    }
+    if (isExist) return res.status(400).json({ msg: 'User already exists' });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ ...req.body, password: hashedPassword });
@@ -25,7 +27,30 @@ authController.post('/register', async (req, res) => {
     return res.status(200).json({ token, user: others });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ msg: 'Server error' });
+    return res.status(500).json({ msg: 'Server error in register' });
+  }
+});
+
+authController.post('/login', async (req, res) => {
+  try {
+    if (req.body.email === '' || req.body.password === '') {
+      return res.status(400).json({ msg: 'All fields are required' });
+    }
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(500).json({ msg: 'Email or password is incorrect' });
+
+    const comparePath = await bcrypt.compare(req.body.password, user.password);
+    if (!comparePath) return res.status(500).json({ msg: 'Incorrect password' });
+
+    const { password, ...others } = user._doc;
+    const token = createToken(others);
+
+    return res.status(200).json({ token, others });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: 'Server error in login' });
   }
 });
 
