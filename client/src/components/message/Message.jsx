599253e12d7e94d4a4e5a48530398c113bfb95a1
Kyryl <bodakvakyryl@gmail.com>
2024-09-23 15:54:58 +0300

    Updated message component

--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { format } from 'timeago.js';
 import avatar from '../../assets/avatar.png';
 import classes from './message.module.css';
 
-const Message = () => {
+const Message = ({ own, message }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [message]);
+
   return (
-    <div className={classes.container}>
-      <div className={classes.wrapper}>
-        <img src={avatar} alt='avatar' className={classes.personImg} />
-        <div className={classes.messageAndTimeAgo}>
-          <div className={classes.text}>Hey, how are you?</div>
-          <span className={classes.timeAgo}>2 hours ago</span>
+    <>
+      {!own && (
+        <div className={classes.container}>
+          <div className={classes.wrapper}>
+            <img src={avatar} alt='avatar' className={classes.personImg} />
+            <div className={classes.messageAndTimeAgo}>
+              <div className={classes.text}>{message.messageText}</div>
+              <span className={classes.timeAgo}>{format(message.createdAt)}</span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+      {own && (
+        <div className={`${classes.container} ${classes.own}`}>
+          <div className={classes.wrapper}>
+            <div className={classes.messageAndTimeAgo}>
+              <div className={classes.text}>{message.messageText}</div>
+              <span className={classes.timeAgo}>{format(message.createdAt)}</span>
+            </div>
+            <img src={avatar} alt='avatar' className={classes.personImg} />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
